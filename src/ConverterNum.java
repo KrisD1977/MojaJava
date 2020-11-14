bb5c4d26a28d77d80697f5b1b6a1ecac6bb2844c
KrisD1977 <derela@student.wszib.edu.pl>
2020-11-14 22:16:39 +0100

    lab7

--- src/ConverterNum.java
+++ src/ConverterNum.java
@@ -1,28 +1,40 @@
+import java.util.Scanner;
+
 public class ConverterNum {
 
     public static void main(String[] args) {
 
-        String bin = "1101";
+        /*String bin = "1101";*/
+
+        System.out.print("Wpisz liczbę w systemie dwójkowym, n =");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String bin = scanner.next();
+
         Long result = 0L;
 
-         // 1        1      0     1   //liczba binarna
+        // 1        1      0     1   //liczba binarnie
         //  0        1     2       3    // indeks (i)
         //  3        2     1       0 // potęgi 2 - odwrócony indeks
         // 1*2^3   1*2^2   0*2^1  1*2^0
 
+       /* System.out.println(bin.length());
+        System.out.println(bin.charAt(2));*/
+
         for (int i = 0; i < bin.length(); i++) {
 
-            char c = bin.charAt(i);
-            int bit = Character.getNumericValue(c);
-            int reverseIndex = bin.length() - i -1;
+            char c = bin.charAt(i); // /*System.out.println((int)c);*/ wartość dziesiętna 1 to 49 itp.
+            int bit = Character.getNumericValue(i);
+            int reverseIndex = bin.length() - i - 1;
 
-            System.out.println("i=" +i + " bit:" + bit + " ri: " + reverseIndex);
+            System.out.println(" i=" + i + " bit:" + bit + " potęga liczby 2: " + reverseIndex);
 
-            result += bit * (long)Math.pow(2, reverseIndex);
+            result += bit * (long) Math.pow(2, reverseIndex);
 
-            System.out.println("Bin:" + bin + " to dziesiętnie:" + result);
         }
 
+        System.out.println("Binarnie:" + bin + " to dziesiętnie:" + result);
 
     }
 }
---
+++ src/ShowMatrix.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ShowMatrix {
+
+    public static void main(String[] args) {
+
+        System.out.print("n =");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if (n > 30) {
+            System.out.println("To za dużo, wybierz liczbę od 1 do 30.");
+
+        } else {
+
+            System.out.println(" n= " + n + " matrix");
+            System.out.println("-------------------");
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 1; j <= n  ; j++) {
+
+
+                    System.out.print((i +j) + " ");
+
+                }
+                System.out.println();
+
+
+            }
+        }
+    }
+}
