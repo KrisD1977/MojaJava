2d059f6bf5024672d58c0b84ed41fd9140484606
KrisD1977 <derela@student.wszib.edu.pl>
2020-12-05 14:40:36 +0100

    05.12.2020 ucze sie

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("HelloWorld!");
+        for (int i = 0; i < 10; i++) {
 
+            System.out.println("HelloWorld!");
+        }
 
     }
 }
